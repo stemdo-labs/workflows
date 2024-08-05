f4dfb8edad6c1c25de6256810e3ade2c2b124f91
RalonsoS <ralonso@stemdo.io>
2024-08-05 11:21:39 +0200

    try with js script

---
+++ set-private-repo.js
@@ -0,0 +1,23 @@
+// Script set private repos
+
+const axios = require('axios');
+
+const [,, repo, org] = process.argv;
+const token = process.env.TOKEN;
+
+const config = {
+  headers: {
+    'Authorization': `token ${token}`,
+    'Accept': 'application/vnd.github.v3+json'
+  }
+};
+
+axios.put(`https://api.github.com/repos/${org}/${repo}`, {
+  private: true
+}, config)
+  .then(response => {
+    console.log(`Successful`);
+  })
+  .catch(error => {
+    console.error(`Error: ${error.message}`);
+  });
