18957ade92f76d284fda160b271002d411deb03c
icasado01 <166407751+icasado01@users.noreply.github.com>
2024-08-07 13:36:27 +0200

    fix script

--- modify-permissions.js
+++ modify-permissions.js
@@ -1,23 +1,21 @@
 const axios = require('axios');
-
 const fs = require('fs');
 const path = require('path');
 
 const reposFilePath = path.join(process.cwd(), 'repos.json');
 const repos = JSON.parse(fs.readFileSync(reposFilePath, 'utf8'));
 
+const token = process.env.TOKEN;
+const org = 'stemdo-labs'; // Organización
+
 let summary = '### Summary of Permission Modifications\n\n';
 summary += '| Repo | User | Status | Message |\n';
 summary += '|------|------|--------|---------|\n';
 
-const token = process.env.TOKEN;
-
-let results = [];
-
 const updatePermissions = async () => {
   for (const repo of repos) {
     try {
-      const url = `https://api.github.com/repos/${repo}/collaborators`;
+      const url = `https://api.github.com/repos/${org}/${repo}/collaborators`;
 
       // Obtener los colaboradores actuales
       const { data: collaborators } = await axios.get(url, {
@@ -31,7 +29,7 @@ const updatePermissions = async () => {
         try {
           // Actualizar permisos a lectura
           await axios.put(
-            `https://api.github.com/repos/${repo}/collaborators/${collaborator.login}`,
+            `https://api.github.com/repos/${org}/${repo}/collaborators/${collaborator.login}`,
             { permission: 'read' },
             {
               headers: {
